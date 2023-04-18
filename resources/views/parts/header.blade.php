e1126e787e6823e1d5beded4ad95d2a178ce5143
Thanthar Hnin Ei <thanthark88@gmail.com>
2023-04-18 12:43:03 +0630

    home page

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,96 @@
+<body id="bg">
+    <div class="page-wraper">
+    <div id="loading-icon-bx"></div>
+        <!-- Header Top ==== -->
+        <header class="header rs-nav">
+            <div class="top-bar">
+                <div class="container">
+                    <div class="row d-flex justify-content-between">
+                        <div class="topbar-left">
+                            <ul>
+                                <li><a href="javascript:;"><i class="fa fa-envelope-o"></i>Support@website.com</a></li>
+                            </ul>
+                        </div>
+                        <div class="topbar-right">
+                            <ul>
+                                
+                                <li><a href="login.html">Login</a></li>
+                                <li><a href="register.html">Register</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="sticky-header navbar-expand-lg">
+                <div class="menu-bar clearfix">
+                    <div class="container clearfix">
+                        <!-- Header Logo ==== -->
+                        <div class="menu-logo">
+                            <a href="index.html">
+                                
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <i class="fa-solid fa-globe fa-2x mr-2"></i>
+                                    </div>
+                                    <div class="align-items-baseline">
+                                        <h4 class="m-0">LanguageTube</h4>
+                                        <span class="m-0">Education & Courses</span>
+                                    </div>
+                                </div>
+    
+                            </a>
+                        </div>
+                        <!-- Mobile Nav Button ==== -->
+                        <button class="navbar-toggler collapsed menuicon justify-content-end" type="button" data-toggle="collapse" data-target="#menuDropdown" aria-controls="menuDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </button>
+                    
+                        <!-- Search Box ==== -->
+                        <div class="nav-search-bar">
+                            <form action="#">
+                                <input name="search" value="" type="text" class="form-control" placeholder="Type to search">
+                                <span><i class="ti-search"></i></span>
+                            </form>
+                            <span id="search-remove"><i class="ti-close"></i></span>
+                        </div>
+                        <!-- Navigation Menu ==== -->
+                        <div class="menu-links navbar-collapse collapse justify-content-end" id="menuDropdown">
+                            <div class="menu-logo">
+                                <a href="index.html">
+                                
+                                    <div class="d-flex align-items-center">
+                                        <div>
+                                            <i class="fa-solid fa-globe fa-2x mr-2"></i>
+                                        </div>
+                                        <div class="align-items-baseline">
+                                            <h5 class="m-0">LanguageTube</h5>
+                                            <span class="m-0"><small>Education & Courses</small></span>
+                                        </div>
+                                    </div>
+        
+                                </a>
+                            </div>
+                            <ul class="nav navbar-nav">	
+                                <li class="active">
+                                    <a href="index-2.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="courses.html">All Courses</a>
+                                </li>
+                                <li>
+                                    <a href="about-2.html">About</a>
+                                </li>
+                                <li>
+                                    <a href="contact-1.html">Contact Us</a>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                        <!-- Navigation Menu END ==== -->
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Header Top END ==== -->
